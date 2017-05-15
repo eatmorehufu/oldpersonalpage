07c85ab564c9d819edbef7b3b5c373b0b789a470
Dean Hu <hhu.hufu@gmail.com>
2017-05-15 00:47:16 -0400

    American psycho accuracy, card flipping

--- js/cardAnimations.js
+++ js/cardAnimations.js
@@ -3,5 +3,12 @@ $(document).ready(() => {
   cards.each((idx, card) => {
     const $card = $(card);
     $card.addClass(`previous${idx}`);
+    $card.click((e) => {
+      $card.addClass("fold-out");
+      window.setTimeout(() => {
+        $card.removeClass("fold-out");
+        $card.toggleClass("previous-focus");
+      }, 500);
+    });
   });
 });
